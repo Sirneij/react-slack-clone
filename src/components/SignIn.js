40eede55a4063f2e3cd3a39db782a2628c10a84e
John Idogun <sirneij@gmail.com>
2020-10-14 13:44:18 +0100

    Added routing and css files

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,11 +2,18 @@ import React from "react";
 
 export default function SignIn() {
   return (
-    <div>
-      <h1>Sign in or up</h1>
-      <form className="form">
-        <button>Sign in or up with Google</button>
-      </form>
-    </div>
+    <main className="channel-container">
+      <section className="form-container">
+        <form action="." className="create-form">
+          <h3 className="form-header centered">Sign in or Create an Account</h3>
+          <small className="brief-description">
+            Signin into your account or register using your Google account.
+          </small>
+          <button type="submit" className="btn btn-primary btn-small-margin">
+            Signin or Register with Google
+          </button>
+        </form>
+      </section>
+    </main>
   );
 }
